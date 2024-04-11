bc783f08962a83113fb5d70bf4bdbbdd79739fd5
AmandaMallet <amandaramosmallet@gmail.com>
2024-04-11 09:24:31 -0300

    mais alterações home

--- home.php
+++ home.php
@@ -6,6 +6,7 @@
     <title>Home</title>
     <script src="https://kit.fontawesome.com/1317d874ee.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="estiloHome.css" media="screen"/>
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&display=swap');
     </style>
@@ -16,68 +17,147 @@
                 <img src="imagens/senai-branco.png" alt="Minha Figura" width="250" height="auto">
         </div>
         <div class="menu-header">
-            <a href="perfil.php">Perfil</a>
-            <p>|</p>
-            <a href="index.php">Sair <i class="fa-solid fa-right-from-bracket"></i></a>
+            <a href="perfil.php">
+                <div class="circulo">
+                        <i class="fa-solid fa-user"></i>
+                </div>
+            </a>
+            <a href="index.php"><i class="fa-solid fa-right-from-bracket"></i></a>
             
         </div>
     </header>
-    <script>
-        let arrow = document.querySelectorAll(".arrow");
-        for (var i = 0; i < arrow.length; i++) {
-            arrow[i].addEventListener("click", (e)=>{
-        let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
-        arrowParent.classList.toggle("showMenu");
-        });
-        }
-    </script>
-   
-    <input type="checkbox" id="check">
-    <label for="check">
-        <i class="fas fa-bars" id="btn"></i>
-        <i class="fa-solid fa-square-xmark" id="cancel"></i>
-    </label>
-    <div class="sidebar">
-        <header>Menu</header>
-        <ul>
-            <li class="menu-item">
-                <div class="menu-item-title">
-                    <a href="#" class="active">
-                        <i class="fa-solid fa-truck-front"></i>
-                        <span>Recebimento</span>  
-                    </a>
-                    <i class="fa-solid fa-chevron-down"></i>
-                </div>  
-                <ul class="sub-menu">
-                    <li class="sub-menu-item">
-                        <a href="#">
-                            <span class="sub-menu-title">Grid</span>
-                        </a>
-                    </li>
-                    <li class="sub-menu-item">
-                    <a href="#">
-                            <span class="sub-menu-title">Grid</span>
-                        </a>
-                    </li>
-                </ul>
-            </li>
+    <div class="sidebar close">
+      
+      <div class="logo-details">
+        
+        <i class="bx bx-menu"></i>
+        <span class="logo_name">Menu</span>
+     
+      </div>
+      
+      <ul class="nav-links">
+        
+        <li>
+          
+          <div class="iocn-link">
+            
+            <a href="#">
+                <i class="fa-solid  fa-truck-front"></i>
+              <span class="link_name">Recebimento</span>
+            </a>
+            
+            <i class='bx bxs-chevron-down arrow' ></i>
+          
+          </div>
+          
+          <ul class="sub-menu">
+            
+            <li><a class="link_name" href="#">Recebimento</a></li>
+            <li><a href="#">Container</a></li>
+            <li><a href="#">Carga</a></li>
+          
+          </ul>
+          
+        </li>
+        
+        <li>
+          
+          <div class="iocn-link">
+            
+            <a href="#">
+            <i class="fa-solid fa-truck-ramp-box"></i>
+              <span class="link_name">Movimentação</span>
+            </a>
+            
+            <i class='bx bxs-chevron-down arrow' ></i>
+          
+          </div>
+          
+          <ul class="sub-menu">
+          
+            <li><a class="link_name" href="#">Movimentação</a></li>
+            <li><a href="#">Exemplo</a></li>
+            <li><a href="#">Exemplo</a></li>
+          
+          </ul>
 
+        </li>
 
-            <li class="menu-item">
-                <a href="#" class="active">
-                    <i class="fas fa-bars"></i>
-                    <span>Painel</span>  
-                </a>
-            </li>
-            <li class="menu-item">
-                <a href="#" class="active">
-                    <i class="fas fa-bars"></i>
-                    <span>Painel</span>  
-                </a>
-            </li>  
-        </ul>    
+        <li>
+          
+          <a href="#">
+            <i class="fa-solid fa-warehouse"></i>
+            <span class="link_name">Estoque</span>
+          </a>
+
+          <ul class="sub-menu blank">
+            <li><a class="link_name" href="#">Estoque</a></li>
+          </ul>
+
+        </li>
+        
+        <li>
+          
+          <a href="#">
+          <i class="fa-solid fa-box"></i>
+            <span class="link_name">Picking</span>
+          </a>
+
+          <ul class="sub-menu blank">
+            <li><a class="link_name" href="#">Picking</a></li>
+          </ul>
+
+        </li>
+
+        <li>
+          
+          <div class="iocn-link">
+          
+            <a href="#">
+            <i class="fa-solid fa-truck-fast"></i>
+              <span class="link_name">Expedição</span>
+            </a>
+            
+            <i class='bx bxs-chevron-down arrow' ></i>
+            
+          </div>
+          
+          <ul class="sub-menu">
+            <li><a class="link_name" href="#">Expedição</a></li>
+            <li><a href="#">Exemplo</a></li>
+            <li><a href="#">Exemplo</a></li>
+            <li><a href="#">Exemplo</a></li>
+          </ul>
+
+        </li>
+        
+        <li>
+        
+          <a href="#">
+          <i class="fa-solid fa-server"></i>
+            <span class="link_name">Controle</span>
+          </a>
+          
+          <ul class="sub-menu blank">
+            <li><a class="link_name" href="#">Controle</a></li>
+          </ul>
+        </li>
+        <li>
+          <a href="#">
+          <i class="fa-regular fa-file"></i>
+            <span class="link_name">Relatório</span>
+          </a>
+          
+          <ul class="sub-menu blank">
+            <li><a class="link_name" href="#">Relatório</a></li>
+          </ul>
+        </li>      
+      </ul><!--Fecha ul-->
     </div>      
-    <div class="conteudo">      
+    <div class="conteudo"> 
+        <div class="titulo-conteudo">    
+         <h1>Bem vindo aluno(a)...</h1>
+        </div>
     </div>
     <footer>
         <div class="linha-footer"><div>
@@ -86,5 +166,24 @@
             3º ano da Turma de desenvolvimento de sistemas do Sesi</p>
         </center>
     </footer>
+
+    <script>
+  
+    let arrow = document.querySelectorAll(".arrow");
+    for (var i = 0; i < arrow.length; i++) {
+      arrow[i].addEventListener("click", (e)=>{
+     let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+     arrowParent.classList.toggle("showMenu");
+      });
+    }
+    
+    let sidebar = document.querySelector(".sidebar");
+    let sidebarBtn = document.querySelector(".bx-menu");
+    console.log(sidebarBtn);
+    sidebarBtn.addEventListener("click", ()=>{
+      sidebar.classList.toggle("close");
+    });
+
+</script>
 </body>
 </html>
