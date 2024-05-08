af88e8a02e375a6d6d2f46cca1272ea3674c88d8
Gabrielly Zgoda <gabyzgoda2007@gmail.com>
2024-05-08 10:33:52 -0300

    foiiiii

--- alunos.php
+++ alunos.php
@@ -145,33 +145,6 @@ $resultado = $conexao->query($sql);
          <h1>Sua lista de alunos :)</h1>
         </div>
       <div class="quadro-alunos">
-          <table>
-              <thead> 
-                <tr>
-                    <th>Alunos</th>
-                    <th>Senhas</th>
-                    <th>Cargos</th>
-                </tr>
-              </thead>
-              <tbody>
-              <form class="form" method="post" action="metodoAlunos.php" id="formlogin" name="formlogin" >
-                  <?php
-                      while($user_data = mysqli_fetch_assoc($resultado))
-                      {
-                          echo "<tr>";
-                          echo "<td>".$user_data['nome']."</td>";
-                          echo "<td>".$user_data['senha']."</td>";
-                          echo "<td>".$user_data['cargo']."</td>";
-                          echo "<td> 
-                          <button onclick(gerarSenha)/>
-                          </td>";
-                          echo "</tr>";
-                      }
-                  ?>
-              </tbody>
-          </table>
-      </div>
-    </div>
     <div>
         <table>
             <thead> 
@@ -182,7 +155,6 @@ $resultado = $conexao->query($sql);
             </tr>
             </thead>
             <tbody>
-            <form class="form" method="post" action="metodoAlunos.php" id="formlogin" name="formlogin" >
                 <?php
                     while($user_data = mysqli_fetch_assoc($resultado))
                     {
@@ -191,18 +163,22 @@ $resultado = $conexao->query($sql);
                         echo "<td>".$user_data['senha']."</td>";
                         echo "<td>".$user_data['cargo']."</td>";
                         echo "<td> 
-                        <a class='' href='resetar.php?id=$user_data'> <a/>
+                        <a class='' href='resetar.php?id=$user_data[id]'> <a/>
                         </td>";
                         echo "<td> 
-                        <button onclick(deletar)/>
+                        <a class='' href='delete.php?id=$user_data[id]'> 
+                        <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash-fill' viewBox='0 0 16 16'>
+                          <path d='M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0'/>
+                        </svg>
+                        <a/>
                         </td>";
                         echo "</tr>";
                     }
                 ?>
-              </form>
             </tbody>
         </table>
     </div>
+    <footer>
         <div class="linha-footer"><div>
         <center>
             <p>Gabrielly, Chris, Julia e Amanda</br>
