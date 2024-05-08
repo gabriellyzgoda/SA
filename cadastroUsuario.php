a7d0871716c376bde196582f825317b74ba211bc
Julía Sartor <julia_sardanha@estudante.sesisenai.org.br>
2024-05-08 07:58:35 -0300

    aaaaaa

--- cadastroUsuario.php
+++ cadastroUsuario.php
@@ -1,7 +1,7 @@
 <?php
 			$hostname = "127.0.0.1";
 			$user = "root";
-			$password = "usbw";
+			$password = "";
 			$database = "sa";
 		
 			$conexao = new mysqli($hostname,$user,$password,$database);
