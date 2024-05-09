6cf2cf87d8cb8d00272e2091f4eff005318e5b90
Gabrielly Zgoda <gabyzgoda2007@gmail.com>
2024-05-09 08:55:43 -0300

    09/05

--- salvar.php
+++ salvar.php
@@ -4,9 +4,9 @@
 
     if(isset($_POST['update'])){
         $id = $_POST['id'];
-        $nome = mysqli_real_escape_string($conexao, $_POST['nome']);
-        $senha = mysqli_real_escape_string($conexao, $_POST['senha']);
-        $cargo = mysqli_real_escape_string($conexao, $_POST['cargo']);
+        $nome = $_POST['nome'];
+        $senha = $_POST['senha'];
+        $cargo = $_POST['cargo'];
 
         $sqlUpdate = "UPDATE cadastro SET nome='$nome', senha='$senha', cargo='$cargo'
         WHERE id='$id'";
