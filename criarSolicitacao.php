0eff69071cc3dd09b0116dffaf95c801a8de8ab3
AmandaMallet <amandaramosmallet@gmail.com>
2024-06-04 12:27:47 -0300

    alterando criar solicitacoes

---
+++ criarSolicitacao.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+session_start();
+include_once('config.php');
+
+// Verifica se o usuário está logado
+if(!isset($_SESSION['email'])) {
+    header("Location: login.php?erro=false");
+    exit;
+}
+$sql = "SELECT * FROM pedidos";
+// puxa conexão
+$resultado = $conexao->query($sql);
+
+// puxa conexão
+?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Professor</title>
+    <script src="https://kit.fontawesome.com/1317d874ee.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="estiloCriarSolicitacao.css" media="screen"/>
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&display=swap');
+    </style>
+</head>
+<body>
+    <header class="header-topo">
+        <div class="logo">
+          <a href="homeP.php"><img src="imagens/senai-branco.png" alt="Minha Figura" width="250" height="auto"></a>
+        </div>
+        <div class="menu-header">
+            <a href="perfil.php">
+                <div class="circulo">
+                        <i class="fa-solid fa-user"></i>
+                </div>
+            </a>
+            <a href="index.php"><i class="fa-solid fa-right-from-bracket"></i></a>      
+        </div>
+    </header>
+    <div class="sidebar close">
+      
+      <div class="logo-details">
+        
+        <i class="bx bx-menu"></i>
+        <span class="logo_name">Menu</span>
+     
+      </div>
+      
+      <ul class="nav-links">
+        
+        <li>
+          
+          <div class="iocn-link">
+            
+            <a href="#">
+            <i class="fa-solid fa-box"></i>
+              <span class="link_name">Pedidos</span>
+            </a>
+            
+            <i class='bx bxs-chevron-down arrow' ></i>
+          
+          </div>
+          
+          <ul class="sub-menu">
+            
+            <li><a class="link_name" href="#">Pedidos</a></li>
+            <li><a href="meuspedidos.php">Meus pedidos</a></li>
+            <li><a href="criarpedido.php">Criar pedido</a></li>
+          
+          </ul>
+          
+        </li>
+        
+        <li>
+          
+          <div class="iocn-link">
+            
+            <a href="#">
+            <i class="fa-solid fa-receipt"></i>
+              <span class="link_name">Nota fiscal</span>
+            </a>
+            
+            <i class='bx bxs-chevron-down arrow' ></i>
+          
+          </div>
+          
+          <ul class="sub-menu">
+          
+            <li><a class="link_name" href="#">Nota fiscal</a></li>
+            <li><a href="criardanfe.php">Criar Danfe</a></li>
+            <li><a href="minhadanfe.php">Minhas Danfe's</a></li>
+          
+          </ul>
+
+        </li>
+
+        <li>
+          
+          <a href="controleP.php">
+            <i class="fa-solid fa-warehouse"></i>
+            <span class="link_name">Controle</span>
+          </a>
+
+          <ul class="sub-menu blank">
+            <li><a class="link_name" href="controleP.php">Controle</a></li>
+          </ul>
+
+        </li>
+        
+        <li>
+          
+          <a href="alunos.php">
+          <i class="fa-solid fa-users"></i>
+            <span class="link_name">Alunos</span>
+          </a>
+
+          <ul class="sub-menu blank">
+            <li><a class="link_name" href="alunos.php">Alunos</a></li>
+          </ul>
+
+        </li>
+
+        <li>
+          
+          <div class="iocn-link">
+          
+            <a href="relatoriosP.php">
+            <i class="fa-solid fa-file-lines"></i>
+              <span class="link_name">Relatórios</span>
+            </a>
+            
+          </div>
+          
+          <ul class="sub-menu">
+            <li><a class="link_name" href="relatoriosP.php">Relatórios</a></li>
+          </ul>
+        </li>  
+        <li>
+          
+          <div class="iocn-link">
+            
+            <a href="#">
+            <i class="fa-solid fa-receipt"></i>
+              <span class="link_name">Solicitações</span>
+            </a>
+            
+            <i class='bx bxs-chevron-down arrow' ></i>
+          
+          </div>
+          
+          <ul class="sub-menu">
+          
+            <li><a class="link_name" href="#">Solicitações</a></li>
+            <li><a href="criarSolicitacao.php">Criar Solicitação</a></li>
+            <li><a href="solicitacoes.php">Minhas Solicitações</a></li>
+          
+          </ul>
+
+        </li>   
+      </ul><!--Fecha ul-->
+    </div>     
+    <div class="conteudo"> 
+        <div class="titulo-conteudo">    
+         <h1>Criação de Solicitação de Produto</h1>
+        </div>
+        <div class="criacao-solicitacao">
+          <div class="bloco-criacao-solicitacao">
+              <form class="form" method="post" action="" id="formPedido">
+                    <div class="form-numero">
+                      <label>Solicitação nº:</label>
+                      <input type="number" name="pedido">
+                    </div>
+                <table>
+                  <thead>
+                    <tr>
+                        <th><p></p></th>
+                        <th><p>Produtos:</p></th>
+                        <th><p>UN</p></th>
+                        <th><p>QTD</p></th>
+                        <th><p>R$/Un</p></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                      <tr>
+                        <td><div class="quadrado-numero-produto">1</div></td>
+                          <td><input type="text" name="produto"></td>
+                          <td><input type="text" name="unidades"></td>
+                          <td><input type="number" name="quantidades"></td>
+                          <td><input type="text" name="valor"></td>
+                      </tr> 
+                      <tr>
+                          <td><div class="quadrado-numero-produto">2</div></td>
+                          <td><input type="text" name="produto2"></td>
+                          <td><input type="text" name="unidades2"></td>
+                          <td><input type="number" name="quantidades2"></td>
+                          <td><input type="text" name="valor2"></td>
+                      </tr>
+                      <tr>
+                          <td><div class="quadrado-numero-produto" >3</div></td>
+                          <td><input type="text" name="produto3"></td>
+                          <td><input type="text" name="unidades3"></td>
+                          <td><input type="number" name="quantidades3"></td>
+                          <td><input type="text" name="valor3"></td>
+                      </tr>
+                      <tr>
+                          <td><div class="quadrado-numero-produto" >4</div></td>
+                          <td><input type="text" name="produto4"></td>
+                          <td><input type="text" name="unidades4"></td>
+                          <td><input type="number" name="quantidades4"></td>
+                          <td><input type="text" name="valor4"></td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <div class="tituloObservacoes">
+                    <p>Observações:</p>
+                  </div>
+                  <div class="blocoObservacoes">
+                  <textarea id="" name="" rows="4" cols="85" placeholder="Digite..."></textarea>
+                  </div>
+                  <div class="blocoFinal">
+                      <input type="submit" value="Enviar">
+                  </div>
+              </form>
+          </div>
+        </div>
+    </div>
+    
+    <footer>
+        <div class="linha-footer"><div>
+        <center>
+            <p>Gabrielly, Letícia, Julia e Amanda</br>
+            3º ano da Turma de desenvolvimento de sistemas do Sesi</p>
+        </center>
+    </footer>
+
+    <script>
+    let arrow = document.querySelectorAll(".arrow");
+    for (var i = 0; i < arrow.length; i++) {
+      arrow[i].addEventListener("click", (e)=>{
+     let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+     arrowParent.classList.toggle("showMenu");
+      });
+    }
+    
+    let sidebar = document.querySelector(".sidebar");
+    let sidebarBtn = document.querySelector(".bx-menu");
+    console.log(sidebarBtn);
+    sidebarBtn.addEventListener("click", ()=>{
+      sidebar.classList.toggle("close");
+    });
+
+</script>
+</body>
+</html>
