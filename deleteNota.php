66093c46730c3a52a23e8673723df4f95f4a9e82
Gabrielly Zgoda <gabyzgoda2007@gmail.com>
2024-08-15 10:44:50 -0300

    15/08

---
+++ deleteNota.php
@@ -0,0 +1,18 @@
+<?php
+if (!empty($_GET['id'])) {
+    include_once('config.php');
+    
+    $id = mysqli_real_escape_string($conexao, $_GET['id']);
+
+    $sqlDelete = "DELETE FROM danfe WHERE id = $id";
+    $resultado = $conexao->query($sqlDelete);
+
+    if ($resultado) {
+        header('Location: minhadanfe.php');
+        } else {
+        echo "Erro ao excluir o pedido: " . $conexao->error;
+    }
+} else {
+    echo "Número do pedido não especificado.";
+}
+header('Location: minhadanfe.php');
