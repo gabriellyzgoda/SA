e78dc2af1665ae4b2e0669ceb948a76282cb8b39
Amanda Mallet <amandaramosmallet@gmail.com>
2024-09-13 11:14:13 -0300

    estilo filtro criar danfe aluno

--- criarNota.php
+++ criarNota.php
@@ -9,175 +9,177 @@ if (!isset($_SESSION['email']) || $_SESSION['professor'] != 0) {
   exit;
 }
 ?>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Aluno</title>
-    <link rel="icon" type="image/x-icon" href="imagens/favicon.ico">
-    <script src="https://kit.fontawesome.com/1317d874ee.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" type="text/css" href="estiloHome.css" media="screen"/>
-    <link rel="stylesheet" type="text/css" href="estiloCriarDanfe.css" media="screen" />
-    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&display=swap');
-    </style>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home Aluno</title>
+  <link rel="icon" type="image/x-icon" href="imagens/favicon.ico">
+  <script src="https://kit.fontawesome.com/1317d874ee.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" type="text/css" href="estiloHome.css" media="screen" />
+  <link rel="stylesheet" type="text/css" href="estiloCriarDanfe.css" media="screen" />
+  <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&display=swap');
+  </style>
 </head>
+
 <body>
-<header class="header-topo">
-        <div class="logSenai">
+  <header class="header-topo">
+    <div class="logSenai">
       <a href="home.php"><img src="imagens/logo-logsenai.png" alt="Minha Figura" width="75" height="auto"></a>
     </div>
     <div class="logo">
       <a href="home.php"><img src="imagens/senai-branco.png" alt="Minha Figura" width="250" height="auto"></a>
     </div>
-        <div class="menu-header">
-          <div class="dropdown-perfil">
-            <a href="#" >
-                <div class="circulo">
-                        <i class="fa-solid fa-user"></i>
-                </div>
-            </a>
-            <div class="dropdown-content">
-              <div class="dropdown-section">
-                <h4>Nome:</h4>
-                <p><?php echo $_SESSION['nome'];?></p>
-              </div>
-              <div class="dropdown-section">
-                <h4>Email:</h4>
-                <p><?php echo $_SESSION['email'];?></p>
-              </div>
-              <div class="dropdown-section">
-                <h4>Cargo:</h4>
-                <p><?php echo $_SESSION['cargo'];?></p>
-              </div>
-            </div>
+    <div class="menu-header">
+      <div class="dropdown-perfil">
+        <a href="#">
+          <div class="circulo">
+            <i class="fa-solid fa-user"></i>
           </div>
-          <a href="sair.php"><i class="fa-solid fa-right-from-bracket"></i></a>      
+        </a>
+        <div class="dropdown-content">
+          <div class="dropdown-section">
+            <h4>Nome:</h4>
+            <p><?php echo $_SESSION['nome']; ?></p>
+          </div>
+          <div class="dropdown-section">
+            <h4>Email:</h4>
+            <p><?php echo $_SESSION['email']; ?></p>
+          </div>
+          <div class="dropdown-section">
+            <h4>Cargo:</h4>
+            <p><?php echo $_SESSION['cargo']; ?></p>
+          </div>
         </div>
-    </header>
-    <div class="sidebar close">
-      
-      <div class="logo-details">
-        
-        <i class="bx bx-menu"></i>
-        <span class="logo_name">Menu</span>
-     
       </div>
-      
-      <ul class="nav-links">
-        
-        <li>
-          
-          <div class="iocn-link">
-            
-            <a href="#">
-                <i class="fa-solid  fa-truck-front"></i>
-              <span class="link_name">Recebimento</span>
-            </a>
-            
-            <i class='bx bxs-chevron-down arrow' ></i>
-          
-          </div>
-          
-          <ul class="sub-menu">
-            
-            <li><a class="link_name" href="#">Recebimento</a></li>
-            <li><a href="container.php">Container</a></li>
-            <li><a href="carga.php">Carga</a></li>
-          <li><a href="pedidodoca.php">Docas</a></li>
-          
-          </ul>
-          
-        </li>
-        
-        <li>
-          
-          <div class="iocn-link">
-            
-            <a href="movimentacao.php">
-            <i class="fa-solid fa-truck-ramp-box"></i>
-              <span class="link_name">Movimentação</span>
-            </a>
-          
-          </div>
-          
-          <ul class="sub-menu">
-          
-            <li><a class="link_name" href="movimentacao.php">Movimentação</a></li>
-          
-          </ul>
+      <a href="sair.php"><i class="fa-solid fa-right-from-bracket"></i></a>
+    </div>
+  </header>
+  <div class="sidebar close">
 
-        </li>
+    <div class="logo-details">
 
-        <li>
-          
-          <a href="estoque.php">
-            <i class="fa-solid fa-warehouse"></i>
-            <span class="link_name">Estoque</span>
-          </a>
+      <i class="bx bx-menu"></i>
+      <span class="logo_name">Menu</span>
 
-          <ul class="sub-menu blank">
-            <li><a class="link_name" href="estoque.php">Estoque</a></li>
-          </ul>
+    </div>
 
-        </li>
-        
-        <li>
-          
-          <a href="picking.php">
-          <i class="fa-solid fa-box"></i>
-            <span class="link_name">Picking</span>
-          </a>
+    <ul class="nav-links">
 
-          <ul class="sub-menu blank">
-            <li><a class="link_name" href="picking.php">Picking</a></li>
-          </ul>
+      <li>
 
-        </li>
-
-         <li>
-        
         <div class="iocn-link">
-        
+
           <a href="#">
-          <i class="fa-solid fa-truck-fast"></i>
-            <span class="link_name">Expedição</span>
+            <i class="fa-solid  fa-truck-front"></i>
+            <span class="link_name">Recebimento</span>
           </a>
-          <i class='bx bxs-chevron-down arrow' ></i>
-          
+
+          <i class='bx bxs-chevron-down arrow'></i>
+
         </div>
-        
+
         <ul class="sub-menu">
-        <li><a href="expedicao.php">Expedição</a></li>
-        <li><a href="vistoriaConferencia.php">Vistoria e Conferência</a></li>
+
+          <li><a class="link_name" href="#">Recebimento</a></li>
+          <li><a href="container.php">Container</a></li>
+          <li><a href="carga.php">Carga</a></li>
+          <li><a href="pedidodoca.php">Docas</a></li>
+
         </ul>
 
       </li>
 
       <li>
-          
-          <div class="iocn-link">
-            
-            <a href="#">
-            <i class="fa-solid fa-receipt"></i>
-              <span class="link_name">Nota fiscal</span>
-            </a>
-            
-            <i class='bx bxs-chevron-down arrow' ></i>
-          
-          </div>
-          
-          <ul class="sub-menu">
-          
-            <li><a href="criarNota.php">Criar Danfe</a></li>
-            <li><a href="minhanota.php">Minhas Danfe's</a></li>
-          
-          </ul>
 
-        </li>
-     
-        
-        <li>
+        <div class="iocn-link">
+
+          <a href="movimentacao.php">
+            <i class="fa-solid fa-truck-ramp-box"></i>
+            <span class="link_name">Movimentação</span>
+          </a>
+
+        </div>
+
+        <ul class="sub-menu">
+
+          <li><a class="link_name" href="movimentacao.php">Movimentação</a></li>
+
+        </ul>
+
+      </li>
+
+      <li>
+
+        <a href="estoque.php">
+          <i class="fa-solid fa-warehouse"></i>
+          <span class="link_name">Estoque</span>
+        </a>
+
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="estoque.php">Estoque</a></li>
+        </ul>
+
+      </li>
+
+      <li>
+
+        <a href="picking.php">
+          <i class="fa-solid fa-box"></i>
+          <span class="link_name">Picking</span>
+        </a>
+
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="picking.php">Picking</a></li>
+        </ul>
+
+      </li>
+
+      <li>
+
+        <div class="iocn-link">
+
+          <a href="#">
+            <i class="fa-solid fa-truck-fast"></i>
+            <span class="link_name">Expedição</span>
+          </a>
+          <i class='bx bxs-chevron-down arrow'></i>
+
+        </div>
+
+        <ul class="sub-menu">
+          <li><a href="expedicao.php">Expedição</a></li>
+          <li><a href="vistoriaConferencia.php">Vistoria e Conferência</a></li>
+        </ul>
+
+      </li>
+
+      <li>
+
+        <div class="iocn-link">
+
+          <a href="#">
+            <i class="fa-solid fa-receipt"></i>
+            <span class="link_name">Nota fiscal</span>
+          </a>
+
+          <i class='bx bxs-chevron-down arrow'></i>
+
+        </div>
+
+        <ul class="sub-menu">
+
+          <li><a href="criarNota.php">Criar Danfe</a></li>
+          <li><a href="minhanota.php">Minhas Danfe's</a></li>
+
+        </ul>
+
+      </li>
+
+
+      <li>
 
         <div class="iocn-link">
 
@@ -197,71 +199,85 @@ if (!isset($_SESSION['email']) || $_SESSION['professor'] != 0) {
         </ul>
 
       </li>
-        <li>
-          <a href="relatorios.php">
+      <li>
+        <a href="relatorios.php">
           <i class="fa-solid fa-file"></i>
-            <span class="link_name">Relatório</span>
-          </a>
-          
-          <ul class="sub-menu blank">
-            <li><a class="link_name" href="relatorios.php">Relatório</a></li>
-          </ul>
-        </li>
-              
-      </ul><!--Fecha ul-->
-    </div>        
-    <center>
+          <span class="link_name">Relatório</span>
+        </a>
+
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="relatorios.php">Relatório</a></li>
+        </ul>
+      </li>
+
+    </ul><!--Fecha ul-->
+  </div>
+  <center>
     <div class="conteudo">
-    <div class="titulo-conteudo">
-      <h1>Criação de Nota Fiscal de Saída</h1>
-    </div>
-    <div class="quadro-conteudo">
-      <div class="bloco-conteudo">
+      <div class="titulo-conteudo">
+        <h1>Criação de Nota Fiscal de Saída</h1>
+      </div>
+      <div class="quadro-conteudo">
+        <div class="bloco-conteudo">
           <div class="chave">
             <form class="form" method="POST" action="criarNota.php">
-            <p>Digite uma data:</p>
-                  <input type="date" id="data" name="data">
-                  <button type="button" id="buscarSolicitacao"><i class="fa-solid fa-magnifying-glass"></i></button>
-                  <br>
-                <p>Digite o Nº da Solicitação</p>
-                <select id="solicitacao" name="solicitacao">
-                          <option value="">Selecione...</option>
-                      </select>
-                <button type="submit">OK</button>
+              <div class="filtros">
+                <div class="linhasFiltro">
+                  <div class="row1Filter">
+                    <p>Digite uma data:</p>
+                  </div>
+                  <div class="row2Filter">
+                    <input type="date" id="data" name="data">
+                    <button type="button" id="buscarSolicitacao"><i class="fa-solid fa-magnifying-glass"></i></button>
+                  </div>
+                </div>
+                <div class="linhasFiltro">
+                  <div class="row1Filter">
+
+                    <p>Nº da Solicitação</p>
+                  </div>
+                  <div class="row2Filter">
+                    <select id="solicitacao" name="solicitacao">
+                      <option value="">Selecione...</option>
+                    </select>
+                    <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+                  </div>
+                </div>
+              </div>
             </form>
             <?php
-            if(isset($_POST['solicitacao'])) {
-              if ($conexao -> connect_errno) {
-                echo "Failed to connect to MySQL: " . $conexao -> connect_error;
+            if (isset($_POST['solicitacao'])) {
+              if ($conexao->connect_errno) {
+                echo "Failed to connect to MySQL: " . $conexao->connect_error;
                 exit();
               } else {
                 // Evita caracteres epsciais (SQL Inject)
-                $solicitacao = $conexao -> real_escape_string($_POST['solicitacao']);
-              
+                $solicitacao = $conexao->real_escape_string($_POST['solicitacao']);
+
                 $sql = "SELECT *
                       FROM solicitacoes
                       WHERE solicitacao = '$solicitacao';";
                 $resultado = $conexao->query($sql);
-                
-                if($resultado->num_rows != 0)
-                {
-                  $row = $resultado -> fetch_array();
-                  ?>
-            <form class="form" method="POST" action="cadastroNota.php">
-            <?php if (isset($_SESSION['erro'])): ?>
-            <p class="error"><?php echo $_SESSION['erro']; unset($_SESSION['erro']); ?></p>
-            <?php endif; ?>
-            <div>
-              <input type="hidden" name="solicitacao" value="<?php echo $row['solicitacao'] ?>">
-              <p>Chave de Acesso:</p>
-              <input class="" type="text" name="id" id="id" size="20" required>
-              <p id="idErro" class="error" style="display: none;">O número da Nota Fiscal já está em uso.</p>
-            </div>
+
+                if ($resultado->num_rows != 0) {
+                  $row = $resultado->fetch_array();
+            ?>
+                  <form class="form" method="POST" action="cadastroNota.php">
+                    <?php if (isset($_SESSION['erro'])): ?>
+                      <p class="error"><?php echo $_SESSION['erro'];
+                                        unset($_SESSION['erro']); ?></p>
+                    <?php endif; ?>
+                    <div>
+                      <input type="hidden" name="solicitacao" value="<?php echo $row['solicitacao'] ?>">
+                      <p>Chave de Acesso:</p>
+                      <input class="" type="text" name="id" id="id" size="20" required>
+                      <p id="idErro" class="error" style="display: none;">O número da Nota Fiscal já está em uso.</p>
+                    </div>
           </div>
           <div class="informacoes">
             <div class="informacoesBloco1">
               <img src="imagens/codigo-barras.png" alt="Minha Figura" width="200" height="auto">
-              <input type="number" name="codbarra" id="codbarra" >
+              <input type="number" name="codbarra" id="codbarra">
             </div>
             <div class="informacoesBloco2">
               <p>Nome/Razão Social: SERVICO NACIONAL DE APRENDIZAGEM INDUSTRIAL SENAI</p>
@@ -317,47 +333,49 @@ if (!isset($_SESSION['email']) || $_SESSION['professor'] != 0) {
               <input type="text" name="ie" value="03.851.105/0001-42" readonly>
             </div>
             <div class="destinatarioBloco3">
-                <label>Valor total da nota:</label>
-                <input type="text" name="total" readonly value="<?php echo $row['totalcompra']; ?>">
+              <label>Valor total da nota:</label>
+              <input type="text" name="total" readonly value="<?php echo $row['totalcompra']; ?>">
             </div>
-            
-      <div class="destinatarioBloco4">
+
+            <div class="destinatarioBloco4">
               <input type="submit" value="Criar" name="criar">
             </div>
-      </form>
+            </form>
       <?php
-                }}}
+                }
+              }
+            }
       ?>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-  </div>
   </center>
-<?php
-include_once('footer.php');
-?>
-    <script>
+  <?php
+  include_once('footer.php');
+  ?>
+  <script>
     document.getElementById('buscarSolicitacao').addEventListener('click', function() {
-    const data = document.getElementById('data').value;
-    if (!data) {
+      const data = document.getElementById('data').value;
+      if (!data) {
         alert('Por favor, selecione uma data.');
         return;
-    }
+      }
 
-    fetch('buscar_solicitacao.php?data=' + encodeURIComponent(data))
+      fetch('buscar_solicitacao.php?data=' + encodeURIComponent(data))
         .then(response => response.json())
         .then(data => {
-            const solicitacaoSelect = document.getElementById('solicitacao');
-            solicitacaoSelect.innerHTML = '<option value="">Selecione...</option>'; // Limpar opções existentes
-            data.forEach(solicitacao => {
-                const option = document.createElement('option');
-                option.value = solicitacao;
-                option.textContent = solicitacao;
-                solicitacaoSelect.appendChild(option);
-            });
+          const solicitacaoSelect = document.getElementById('solicitacao');
+          solicitacaoSelect.innerHTML = '<option value="">Selecione...</option>'; // Limpar opções existentes
+          data.forEach(solicitacao => {
+            const option = document.createElement('option');
+            option.value = solicitacao;
+            option.textContent = solicitacao;
+            solicitacaoSelect.appendChild(option);
+          });
         })
         .catch(error => console.error('Erro ao buscar solicitações:', error));
-  });
+    });
     document.getElementById('id').addEventListener('blur', function() {
       var id = this.value;
       var erroMsg = document.getElementById('idErro');
@@ -366,7 +384,8 @@ include_once('footer.php');
       if (id.trim() === '') {
         erroMsg.style.display = 'none';
         return;
-      }});
+      }
+    });
 
     document.getElementById('id').addEventListener('blur', function() {
       var id = this.value;
@@ -377,8 +396,8 @@ include_once('footer.php');
         erroMsg.style.display = 'none';
         return;
       }
-       // Fazer a solicitação AJAX para verificar o ID
-       var xhr = new XMLHttpRequest();
+      // Fazer a solicitação AJAX para verificar o ID
+      var xhr = new XMLHttpRequest();
       xhr.open('GET', 'verificarNota.php?id=' + encodeURIComponent(id), true);
       xhr.onload = function() {
         if (xhr.status === 200) {
@@ -395,19 +414,19 @@ include_once('footer.php');
 
     let arrow = document.querySelectorAll(".arrow");
     for (var i = 0; i < arrow.length; i++) {
-      arrow[i].addEventListener("click", (e)=>{
-     let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
-     arrowParent.classList.toggle("showMenu");
+      arrow[i].addEventListener("click", (e) => {
+        let arrowParent = e.target.parentElement.parentElement; //selecting main parent of arrow
+        arrowParent.classList.toggle("showMenu");
       });
     }
-    
+
     let sidebar = document.querySelector(".sidebar");
     let sidebarBtn = document.querySelector(".bx-menu");
     console.log(sidebarBtn);
-    sidebarBtn.addEventListener("click", ()=>{
+    sidebarBtn.addEventListener("click", () => {
       sidebar.classList.toggle("close");
     });
-
-</script>
+  </script>
 </body>
+
 </html>
